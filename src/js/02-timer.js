b39e37456220be0adee5c3cef88e4ba667799933
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-08-21 13:45:50 +0200

    task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,7 +10,7 @@ const dataSeconds = document.querySelector('[data-seconds]');
 
 const dateNow = new Date();
 const timeNow = dateNow.getTime()// дата зараз в мілісек чи використати Date.now???
-
+ let countdownInterval = null;
 let timeCurrent = null;
 btnDataStart.disabled = true;
 
@@ -27,34 +27,28 @@ flatpickr("#datetime-picker", {
   onClose(selectedDates) {
     timeCurrent = selectedDates[0].getTime();    // дата обрана користувачев в мілісек
     const time = timeCurrent - timeNow;    // період в мілісек (різниця між поточним та обраним періодом)
-
-    btnDataStart.addEventListener('click', onClickStart)
     
-    function onClickStart(){
-      
-      setInterval(() => {
         if (time < 0) {
-          window.alert("Please choose a date in the future")
-        } else if (time > 0) {
-          convertMs(time)
-          //  форматування мілісек в дн:год:хв:сек
-          console.log(convertMs(time).days);
-          
-           dataDays.textContent = addLeadingZero(convertMs(time).days);
-          dataHours.textContent = addLeadingZero(convertMs(time).hours);
-          dataMinutes.textContent = addLeadingZero(convertMs(time).minutes);
-          dataSeconds.textContent =addLeadingZero(convertMs(time).seconds);
-        } else {
-          dataDays.textContent = '00';
-          dataHours.textContent = '00';
-          dataMinutes.textContent = '00';
-          dataSeconds.textContent = '00';
-        }
-
-      }, 1000);
-    };
-  } ,
-} )  
+          window.alert("Please choose a date in the future");
+          return;
+    }
+      clearInterval(countdownInterval); // Видаляємо попередній інтервал, якщо він існує
+          countdownInterval = setInterval(() => {
+      timeCurrent -= 1000; // Оновлюємо time на 1 секунду
+      if (timeCurrent < timeNow) {
+        clearInterval(countdownInterval); // Зупиняємо таймер, якщо час вичерпаний
+        window.alert("Time's up!");
+        return;
+      }
+       const remainingTime = timeCurrent - timeNow;
+      const timeObject = convertMs(remainingTime);
+      dataDays.textContent = addLeadingZero(timeObject.days);
+      dataHours.textContent = addLeadingZero(timeObject.hours);
+      dataMinutes.textContent = addLeadingZero(timeObject.minutes);
+      dataSeconds.textContent = addLeadingZero(timeObject.seconds);
+    }, 1000);
+  },
+});
 
 
 function convertMs(ms) {
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+const delayEl= document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl= document.querySelector('[name="amount"]');
+
+formEl.addEventListener('submit', handlercreatePromises)
+
+function handlercreatePromises(evt) {
+  evt.preventDefault();
+  let delay = Number(delayEl.value);
+  let step =Number(stepEl.value);
+  let amount = Number(amountEl.value);
+  for (let i = 1; i <= amount; i += 1) {
+        createPromise(i, delay);
+delay += step;
+    
   }
+
 }
+
+function createPromise(position, delay) {
+  const promise = new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay })
+      } else {
+        rej({ position, delay })
+      }
+    }, delay);
+    
+  });
+  promise
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
+
+
+
+
+
